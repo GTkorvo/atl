351b4ba156cfaaf129a3b8c21393a373219b301a
Greg Eisenhauer <eisen@cc.gatech.edu>
2013-05-12 22:50:14 +0000

    Add the ability to have specific mutex lock/unlock functions that protect the global atom server.  This is prep work for removing gen_threads (which has not yet been done).  Kill autotools support in atl.  Also kill kernel build support (so old it probably didn't work anyway), and the long-unused ability to encode attributes as other-than-base64 values.
    Lastly, add a version.c file so that we generate version numbers on atl
    libraries.  Start this with version 2.0.1.
    
    
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/atl/trunk@14504 8ba6a8b9-845b-0410-94f0-974be2f5df47

---
+++ version.c
@@ -0,0 +1,28 @@
+#if defined(FUNCPROTO) || defined(__STDC__) || defined(__cplusplus) || defined(c_plusplus)
+#ifndef ARGS
+#define ARGS(args) args
+#endif
+#else
+#ifndef ARGS
+#define ARGS(args) (/*args*/)
+#endif
+#endif
+
+#include <stdio.h>
+#include "config.h"
+
+static char *ATL_version = "ATL Version 2.0.1 rev. 14459  -- 2013-05-03 07:19:24 -0400 (Fri, 03 May 2013)\n";
+
+#if defined (__INTEL_COMPILER)
+//  Allow extern declarations with no prior decl
+#  pragma warning (disable: 1418)
+#endif
+void ATLprint_version()
+{
+    printf("%s",ATL_version);
+}
+void ATLfprint_version(FILE*out)
+{
+    fprintf(out, "%s",ATL_version);
+}
+
