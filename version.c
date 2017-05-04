a1ba81b201545896dcec2f1bb0605722eaecef7f
Chaos User <chaos@cc.gatech.edu>
2017-05-04 03:50:10 +0000

    Normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/atl/trunk@26803 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *ATL_version = "ATL Version 2.1.1 rev. 26688  -- 2017-04-21 16:51:47 -0400 (Fri, 21 Apr 2017)\n";
+static char *ATL_version = "ATL Version 2.1.2 rev. 26800  -- 2017-05-03 15:09:47 -0400 (Wed, 03 May 2017)\n";
 
 #if defined (__INTEL_COMPILER)
 //  Allow extern declarations with no prior decl
